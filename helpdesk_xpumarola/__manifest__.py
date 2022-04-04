e43f8cadc91be84c4b86d26f9ef1192a772047b9
xpumarolaApen <xpumarola@apen.es>
2022-04-04 16:42:00 +0000

    HelpDesk

---
+++ helpdesk_xpumarola/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Helpdesk Xavier Pumarola',
+    'summary': 'Manage helpdesk tickets',
+    'version': '15.0.1.0.0',
+    'author': 'Xavier Pumarola',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'security/ir.model.access.csv',    
+        'views/helpdesk_ticket_views.xml',    
+    ],
+}
