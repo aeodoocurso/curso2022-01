1dbbcef7d884f6fd901bdd27d9e7d5cb3863b20c
Carlos Pardo <cpardo@sdi.es>
2022-04-05 16:50:10 +0000

    Tarea 02

---
+++ helpdesk_carlospardo/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name" : "Helpdesk Carlos Pardo",
+    "summary" : "Manage helpdesk tickets",
+    "version" : "15.0.1.0.0",
+    "author" : "Carlos Pardo",
+    "depends" : ["base"],
+    "license": "AGPL-3",
+    "data":[
+        "security/helpdesk_security.xml",
+        "security/ir.model.access.csv",
+        "views/helpdesk_ticket_views.xml"
+    ]
+
+}
