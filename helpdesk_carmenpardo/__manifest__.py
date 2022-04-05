415fabaa2c96049d13bf4deb0eb887210f754c1c
Carmen-PL <mcpardo@sdi.es>
2022-04-05 16:15:28 +0000

    Primera tarea

---
+++ helpdesk_carmenpardo/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Helpdesk Carmen Pardo',
+    'summary': 'Manage helpdesk tickets',
+    'version': '15.0.1.0.0',
+    'author': 'Carmen Pardo',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/helpdesk_ticket_views.xml',
+    ],
+} 
