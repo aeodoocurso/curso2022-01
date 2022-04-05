ff83bb9b92f05ce5f9f77b2c075d5e7aca0b1714
MMartinezDA <mmartinez@datanalisis.es>
2022-04-05 16:06:45 +0200

    v1.0

--- helpdesk_miguelmartinez/__manifest__.py
+++ helpdesk_miguelmartinez/__manifest__.py
@@ -1,13 +1,12 @@
-# Copyright 2022 Miguel Martínez
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Helpdesk Miguel Martinez",
     "summary": "Task from Odoo course",
     "version": "15.0.1.0.0",
     "author": "Miguel",
     "license": "AGPL-3",
-    "depends": [
-        "base",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/helpdesk_ticket_views.xml",
     ],
-
 }
