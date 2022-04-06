cc23e367d0581b6f852aa87912592c6d59ecfd97
rubenseijas <rseijas@aira.es>
2022-04-06 14:32:39 +0000

    [ADD] modulo helpdesk

---
+++ helpdesk_rubenseijas/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Helpdek ruben Seijas',
+    'summary': 'Manage helpdesk tickets',
+    'version': '15.0.1.0.0',
+    'author': 'Ruben Seijs',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'views/helpdesk_ticket_views.xml',
+        'security/helpdesk_security.xml',
+        'security/ir.model.access.csv',
+    ],
+} 
