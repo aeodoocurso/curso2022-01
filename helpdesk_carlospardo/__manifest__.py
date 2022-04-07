7e5212f5164d6ddaccaeb1ae903f028bb45a704d
Carlos Pardo <cpardo@sdi.es>
2022-04-07 16:27:54 +0000

    Tarea 01.03

--- helpdesk_carlospardo/__manifest__.py
+++ helpdesk_carlospardo/__manifest__.py
@@ -8,7 +8,7 @@
     "data":[
         "security/helpdesk_security.xml",
         "security/ir.model.access.csv",
-        "views/helpdesk_ticket_views.xml"
-    ]
-
+        "views/helpdesk_ticket_views.xml",
+        "views/helpdesk_ticket_tag_views.xml",
+    ],
 }
