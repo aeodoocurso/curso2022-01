353f9161e7f90dd81508620750ef59e3350f16e6
xpumarolaApen <xpumarola@apen.es>
2022-04-07 15:14:40 +0000

    Añadir Opciones

--- helpdesk_xpumarola/models/helpdesk_ticket.py
+++ helpdesk_xpumarola/models/helpdesk_ticket.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields,models,api
 
 class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
@@ -14,6 +14,10 @@ class HelpdeskTicket(models.Model):
     user_id = fields.Many2one(
         comodel_name='res.users',
         string='Assigned to')
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string='Partner')
+    
     sequence = fields.Integer()
 
     state = fields.Selection(
@@ -26,7 +30,40 @@ class HelpdeskTicket(models.Model):
          ('cancelado','Cancelado')],
          string='State',
          default='nuevo')
+
+    action_ids = fields.One2many(
+        comodel_name='helpdesk.ticket.action',
+        inverse_name='ticket_id',
+        string='Actions Done')
+
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket.tag',
+        # relation='table_name',
+        # column1='col_name',
+        # column2='other_col_name',
+        string='Tags')         
     
         
 
+    def to_asignado(self):
+        self.ensure_one()
+        self.state = 'asignado'
     
+    def to_en_proceso(self):
+        self.write({'state': 'en_proceso'})
+    
+    def to_pendiente(self):
+        for record in self:
+            record.state = 'pendiente'
+    
+    def review_actions(self):
+        self.ensure_one()
+        self.action_ids.review()
+
+        # actions = self.env['helpdesk.ticket.action'].search([('ticket_id', '=', self.id)])
+        # actions.review()
+    
+    @api.model
+    def get_amount_tickets(self):
+        # Give amount of ticket for active user
+        return self.search_count([('user_id', '=', self.env.user.id)])
