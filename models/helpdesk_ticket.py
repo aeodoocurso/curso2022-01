9ae1ec6e3be5938a9ab10854a96828c08f20af02
susana-romero <ysusana.romero@pesol.es>
2022-04-07 08:02:32 +0000

    Curso semana 2 día 3

--- models/helpdesk_ticket.py
+++ models/helpdesk_ticket.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields,models,api
 
 class HelpdeskTicket(models.Model):
     _name = "helpdesk.ticket"
@@ -6,7 +6,7 @@ class HelpdeskTicket(models.Model):
     _order = "sequence"
 
     name = fields.Char(required=True)
-    description = fields.Text()
+    description = fields.Text(translatable=True)
     date = fields.Date()
     limit_date = fields.Date()
     assigned = fields.Boolean(help="Ticket assigned to someone",readonly=True)
@@ -14,6 +14,10 @@ class HelpdeskTicket(models.Model):
     user_id = fields.Many2one(
         comodel_name='res.users',
         string='Assigned to'
+    )
+    partner_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Partner'
     )
     sequence = fields.Integer()
     state = fields.Selection(
@@ -25,3 +29,34 @@ class HelpdeskTicket(models.Model):
          ('cancelado', 'Cancelado')],
         string='State',
         default='nuevo')
+    actions_ids = fields.One2many(
+        comodel_name="helpdesk.ticket.action",
+        inverse_name="ticket.id",
+        string="Actions done"
+    )
+    tag_ids = fields.Many2many(
+        comodel_name = "helpdesk.ticket.tag",
+        string = "Tags"
+    )
+
+    def to_asignado(self):
+        self.ensure_one()
+        self.state='asignado'
+
+    def to_en_proceso(self):
+        self.write({'state':'en_proceso'})
+
+    def to_pendiente(self):
+        for record in self:
+            record.state='pendiente'
+
+    def review_actions(self):
+        self.ensure_one()
+        self.action_ids.review()
+    
+    '''''
+    @api.model
+    def get_amount_tickets(self):
+        return self.search_count()
+
+    '''''
