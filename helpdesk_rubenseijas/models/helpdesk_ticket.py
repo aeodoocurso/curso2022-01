f5df384b6f0efc0c4c19d74383117e4bfc7f6b29
rubenseijas <rseijas@aira.es>
2022-04-27 11:34:20 +0000

    [ADD] TASK-02.02

--- helpdesk_rubenseijas/models/helpdesk_ticket.py
+++ helpdesk_rubenseijas/models/helpdesk_ticket.py
@@ -1,18 +1,27 @@
-from odoo import api, fields, models
+from odoo import api, Command, fields, models, _
 
 class HelpdeskTicket(models.Model):
     _name = "helpdesk.ticket"
     _description = "Helpdesk Ticket"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, copy=False)
     description = fields.Text()
     sequence = fields.Integer()
     date = fields.Date(help="Date when the ticket was created")
     due_date = fields.Datetime(help="Date and time when the ticket will be closed")
     time = fields.Float(string="Time")
-    assigned = fields.Boolean(help="Ticket assigned to someone", readonly=True)
+    assigned = fields.Boolean(help="Ticket assigned to someone")
     actions_todo = fields.Html()
-    user_id = fields.Many2one(comodel_name="res.users", string="Assigned To")
+    user_id = fields.Many2one(
+        comodel_name="res.users", 
+        string="Assigned To",
+        search="_search_assigned",
+        inverse="_set_assigned"
+    )
+    user_email = fields.Char(
+        string="User Email",
+        related="user_id.partner_id.email"
+    )
     state = fields.Selection(
         [('nuevo', 'Nuevo'),
         ('asignado', 'Asignado'),
@@ -23,9 +32,20 @@ class HelpdeskTicket(models.Model):
         string="State", default="nuevo"
     )
     partner_id = fields.Many2one("res.partner", "Customer")
+    partner_email = fields.Char(
+        string="Partner Email",
+        related="partner_id.email"
+    )
     action_ids = fields.One2many(comodel_name="helpdesk.ticket.action", inverse_name="ticket_id")
-    tag_ids = fields.Many2many(comodel_name='helpdesk.ticket.tag', string='Ticket Tags')
-    color = fields.Integer('Color Index')
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket.tag',  # El otro modelo
+        relation="helpdesk_ticket_tag_helpdesk_ticket_rel",  # nombre igual en ambos lados
+        column1="ticket_ids",  # Este objeto
+        column2="tag_ids",  # El otro objeto
+        string='Tags',
+    )
+    color = fields.Integer('Color Index', default=0)
+    tag_name = fields.Char(string="Tag Name")
 
     def review_actions(self):
         self.ensure_one()
@@ -35,3 +55,42 @@ class HelpdeskTicket(models.Model):
     def get_amount_tickets(self):
         # Give amount of ticket for active user
         return self.search_count([('user_id', '=', self.env.user.id)]) 
+
+    def _search_assigned(self, operator, value):
+        if operator not in ['=', '!='] or not isinstance(value, bool):
+            raise ValueError(_('Operation not supported'))
+        if (operator == '=' and value) or (operator != '!=' and not value):
+            new_operator = '!='
+        else:
+            new_operator = '='
+        return [('user_id', new_operator, False)]
+
+    def _set_assigned(self):
+        for record in self:
+            if not record.assigned:
+                record.user_id = False
+            elif not record.user_id:   
+                record.user_id = self.env.user
+
+    def create_and_link_tag(self):
+        self.ensure_one()
+
+        # crear ticket y asignar
+        # tag = self.env['helpdesk.ticket.tag'].create({'name': self.tag_name})
+        # self.write({'tag_ids': [(4, tag.id, 0)]})
+        # self.write({'tag_ids': [Command.link(tag.id)]})
+
+        # crear el ticket desde escritura tag_ids
+        # self.write({
+        #     'tag_ids': [(0, 0, {'name': self.tag_name})],
+        #     'tag_name': False
+        # })
+
+        # crear tag asociado al ticket
+        self.env['helpdesk.ticket.tag'].create({
+            'name': self.tag_name,
+            'ticket_ids': [(4, self.id, 0)],  # [(6, 0, self.ids)] or [(4, self.id, {'name': self.tag_name})] or [Command.set(self.ids)]
+        })
+        self.write({'tag_name': False})
+
+
--- helpdesk_rubenseijas/models/helpdesk_ticket_tag.py
+++ helpdesk_rubenseijas/models/helpdesk_ticket_tag.py
@@ -6,3 +6,10 @@ class HelpdeskTicketTag(models.Model):
 
     name = fields.Char(required=True)
     description = fields.Text()
+    ticket_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket',  # El otro modelo
+        relation="helpdesk_ticket_tag_helpdesk_ticket_rel",  # nombre igual en ambos lados
+        column1="tag_ids",  # Este objeto
+        column2="ticket_ids",  # El otro objeto
+        string='Tickets',
+    )
